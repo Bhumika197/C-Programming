89dece04466b77326aa8b3836ce392c4161c853b
Bhumika197 <chintabhumika07@gmail.com>
2025-01-02 02:28:13 +0530

    Simple C done!!!

--- simpleC.c
+++ simpleC.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.H>
 int main()
+
 {
     int choice;
     do{
-    printf("Welcome to simple Calculator\n");
+    printf("\nWelcome to simple Calculator\n");
     printf("Choose any one of the Operation given below\n");
     printf("\n1. Addition");
     printf("\n2. Substration");
@@ -30,8 +32,6 @@ switch(choice){
             printf("Enter the number %d: ", i);
             scanf("%d",  &array[i]);
             
-            
-        
         }
         for(int i = 1; i <= n; i++) {
         sum += array[i];}
@@ -40,15 +40,82 @@ switch(choice){
              break;
         
         }
-    }
-    
 
-    }
-    while(choice != 7);
+        case 2: {
+            int N1, N2, result; 
+        printf("Enter Number 1: ");
+        scanf("%d", &N1);
+        printf("Enter Number 2: ");
+        scanf("%d", &N2);
+        printf("The Answer is %d", N1-N2);
+        break; 
 
-    
+        }
 
+        case 3: {
+            int i;
+    int n;
+        printf("Enter no of numbers you want to add : ");
+        scanf("%d", &n);
+    int array[n]; // number of number you want to add
+    int product = 1;
+        
+        for(i=1; i<= n; i++){
+            printf("Enter the number %d: ", i);
+            scanf("%d",  &array[i]);
+            
+            
+        
+        }
+        for(int i = 1; i <= n; i++) {
+        product = product* array[i];}
+
+            printf("The Addition of the given numbers is : %d\n", product);
+             break;
+        
+
+        }
+
+        case 4:{//division
+         float N1, N2, result; 
+        printf("Enter Number 1: ");
+        scanf("%lf", &N1);
+        printf("Enter Number 2: ");
+        scanf("%lf", &N2);
+        printf("The Answer is %lf", N1/N2);
+        
+            break; 
+        }
+        case 5:{//modulus
+        int N1, N2, result; 
+        printf("Enter Number 1: ");
+        scanf("%d", &N1);
+        printf("Enter Number 2: ");
+        scanf("%d", &N2);
+        printf("The Answer is %d", N1%N2);
         
     
-    return 0; 
+            break;
+        }
+        case 6:{
+            //power
+            float N1, N2, a; 
+            printf("Enter Number 1: ");
+            scanf("%lf", &N1);
+            printf("Enter Number 2: ");
+            scanf("%lf", &N2);
+a = pow(N1, N2);
+printf("The Answer is %lf", a);
+            break;
+        }
+    
+    
 }
+if(choice > 7){
+        printf("Please enter a valid option!\n");
+    
+    }
+    }
+    while(choice != 7);
+ return 0; 
+}clea
