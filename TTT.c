f576efef539fae184035b049509a130eda8d5284
Bhumika197 <chintabhumika07@gmail.com>
2025-01-06 00:12:22 +0530

    Tick-tak-toe,Edit part1

---
+++ TTT.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int difficulty; 
+int  main()
+{
+    while(1){
+    printf("\nSelect difficulty level ");
+    printf("\n1. Human");
+    printf("\n2. God (Imposible to win)");
+    printf("\n Enter your choice:");
+    scanf("%d", &difficulty); 
+
+
+if(difficulty!= 1 && difficulty!= 2){
+    printf("\nIncorrect choice enter (1/2)!!");
+
+}else{
+    break; 
+}
+}
+
+}
