67512ad2fc1102a696900fdd2b5b24ea8a95832e
Bhumika197 <chintabhumika07@gmail.com>
2025-03-23 13:02:27 +0530

    numbers

--- AnumbersC.c
+++ AnumbersC.c
@@ -1,14 +1,34 @@
 //array numbers comparision; 
 #include <stdio.h>
-int main(){
+
+int main() {
     int n; 
-    scanf("Enter the no  elements in the array %d", &n);
-    int arr[n];
-    int i;
     
-    for(i=0; i<n;i++);{
-        printf("Element array[%d]:" , i++);
-        scanf("%d", &arr[i++]);
-}
-return 0; 
+    
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    
+    if (n <= 0) {
+        printf("Invalid array size.\n");
+        return 1;
+    }
+
+    int arr[n]; 
+    
+
+    int i;
+    for(i = 0; i < n; i++) {
+        printf("Element array[%d]: ", i);
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Array elements are: ");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
