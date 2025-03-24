2ea6c483c62d5532c7138c166983e3ee0d41f8e4
Bhumika197 <chintabhumika07@gmail.com>
2025-03-24 22:42:43 +0530

    hello

--- hello.c
+++ hello.c
@@ -4,6 +4,12 @@ int main(){
 
     printf("Hello worldy");
     scanf("%d",&n);
-    printf("%d", n );
+    printf("%d\n", n );
+    if(n>= 0){
+        printf("the number is a Whole Number!\n");
+
+    }else{
+        printf("The Number is not a whole number");
+    }
     return 0; 
 }
